be5b35bc5d69e538ab051e0beb9cfc80e197cb60
Yann Jouanique <yann.jouanique@gmail.com>
2022-11-17 18:55:10 +0200

    First commit

--- README.md
+++ README.md
@@ -1,47 +1,46 @@
-![Banner image](https://user-images.githubusercontent.com/10284570/173569848-c624317f-42b1-45a6-ab09-f0ea3c247648.png)
+# n8n-nodes-browserless
 
-# n8n-nodes-starter
+[![npm version](https://badge.fury.io/js/n8n-nodes-browserless.svg)](https://badge.fury.io/js/@oneacrefund%2Fn8n-nodes-browserless)
 
-This repo contains example nodes to help you get started building your own custom integrations for [n8n](n8n.io). It includes the node linter and other dependencies.
+This is an n8n community node to control a headless browser using the [Browserless](https://browserless.js.org) library (itself based on [Puppeteer](https://pptr.dev)).
 
-To make your custom node available to the community, you must create it as an npm package, and [submit it to the npm registry](https://docs.npmjs.com/packages-and-modules/contributing-packages-to-the-registry).
+[n8n](https://n8n.io/) is a [fair-code licensed](https://docs.n8n.io/reference/license/) workflow automation platform.
 
-## Prerequisites
+[Installation](#installation)  
+[Operations](#operations)  
+[Credentials](#credentials)
+[Compatibility](#compatibility)  
+[Resources](#resources)  
+[Version history](#version-history)
 
-You need the following installed on your development machine:
+## Installation
 
-* [git](https://git-scm.com/downloads)
-* Node.js and npm. Minimum version Node 16. You can find instructions on how to install both using nvm (Node Version Manager) for Linux, Mac, and WSL [here](https://github.com/nvm-sh/nvm). For Windows users, refer to Microsoft's guide to [Install NodeJS on Windows](https://docs.microsoft.com/en-us/windows/dev-environment/javascript/nodejs-on-windows).
-* Install n8n with:
-	```
-	npm install n8n -g
-	```
-* Recommended: follow n8n's guide to [set up your development environment](https://docs.n8n.io/integrations/creating-nodes/build/node-development-environment/).
+Follow the [installation guide](https://docs.n8n.io/integrations/community-nodes/installation/) in the n8n community nodes documentation.
 
+## Operations
 
-## Using this starter
+* Page:
+  * Take Screenshot
+  * Save as PDF
+  * Get HTML
+  * Get Text
 
-These are the basic steps for working with the starter. For detailed guidance on creating and publishing nodes, refer to the [documentation](https://docs.n8n.io/integrations/creating-nodes/).
+## Credentials
 
-1. [Generate a new repository](https://github.com/n8n-io/n8n-nodes-starter/generate) from this template repository.
-2. Clone your new repo:
-    ```
-    git clone https://github.com/<your organization>/<your-repo-name>.git
-    ```
-3. Run `npm i` to install dependencies.
-4. Open the project in your editor.
-5. Browse the examples in `/nodes` and `/credentials`. Modify the examples, or replace them with your own nodes.
-6. Update the `package.json` to match your details.
-7. Run `npm run lint` to check for errors or `npm run lintfix` to automatically fix errors when possible.
-8. Test your node locally. Refer to [Run your node locally](https://docs.n8n.io/integrations/creating-nodes/test/run-node-locally/) for guidance.
-9. Replace this README with documentation for your node. Use the [README_TEMPLATE](README_TEMPLATE.md) to get started.
-10. Update the LICENSE file to use your details.
-11. [Publish](https://docs.npmjs.com/packages-and-modules/contributing-packages-to-the-registry) your package to npm.
+No credentials for this node
 
-## More information
+## Compatibility
 
-Refer to our [documentation on creating nodes](https://docs.n8n.io/integrations/creating-nodes/) for detailed information on building your own nodes.
+Tested against n8n v0.196.0
 
-## License
+## Resources
 
-[MIT](https://github.com/n8n-io/n8n-nodes-starter/blob/master/LICENSE.md)
+* [n8n community nodes documentation](https://docs.n8n.io/integrations/community-nodes/)
+* [Browserless](https://browserless.js.org)
+* [Puppeteer](https://pptr.dev)
+
+## Version history
+
+### 0.1.0
+
+First Release!
---
+++ nodes/Browserless/Browserless.node.ts
@@ -0,0 +1,307 @@
+import { IExecuteFunctions } from 'n8n-core';
+
+import {
+	IDataObject,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	NodeOperationError,
+} from 'n8n-workflow';
+
+// @ts-ignore
+import createBrowserless from 'browserless';
+
+export class Browserless implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Browserless',
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		documentationUrl: 'https://github.com/one-acre-fund/n8n-nodes-browserless',
+		name: 'browserless',
+		icon: 'file:chrome.svg',
+		group: ['transform'],
+		version: 1,
+		description: 'A node to run a headless browser and take screenshots or PDF',
+		defaults: {
+			name: 'Browserless',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [],
+		properties: [
+			// Resources
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Page',
+						value: 'page',
+						description: 'Manipulate Web Pages',
+					},
+				],
+				default: 'page',
+				required: true,
+			},
+
+			// Operations
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				required: true,
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['page'],
+					},
+				},
+				default: 'screenshot',
+				options: [
+					{
+						name: 'Screenshot',
+						value: 'screenshot',
+						action: 'Take page screenshot',
+					},
+					{
+						name: 'PDF',
+						value: 'pdf',
+						action: 'Save page as pdf',
+					},
+					{
+						name: 'Text',
+						value: 'text',
+						action: 'Save page text',
+					},
+					{
+						name: 'HTML',
+						value: 'html',
+						action: 'Save page html',
+					},
+				],
+			},
+
+			{
+				displayName: 'Target URL',
+				name: 'url',
+				type: 'string',
+				default: '',
+				required: true,
+				displayOptions: {
+					show: {
+						resource: ['page'],
+					},
+				},
+			},
+
+			{
+				displayName: 'Options',
+				name: 'options',
+				type: 'collection',
+				default: {},
+				displayOptions: {
+					show: {
+						resource: ['page'],
+					},
+				},
+				options: [
+					{
+						displayName: 'Browser Name',
+						name: 'device',
+						type: 'string',
+						default: '',
+						description:
+							'Name of browser to emulate - see [Puppeteer docs](https://pptr.dev/next/api/puppeteer.devices) for a list',
+					},
+					{
+						displayName: 'Timeout',
+						name: 'timeout',
+						type: 'number',
+						default: 30000,
+						description: 'Browser Timeout (ms)',
+					},
+					{
+						displayName: 'Width',
+						name: 'width',
+						type: 'number',
+						default: 1440,
+						description: 'Viewport Width (px)',
+					},
+					{
+						displayName: 'Height',
+						name: 'height',
+						type: 'number',
+						default: 900,
+						description: 'Viewport Height (px)',
+					},
+					{
+						displayName: 'User-Agent',
+						name: 'userAgent',
+						type: 'string',
+						default: '',
+						description: 'User Agent',
+					},
+					{
+						displayName: 'Element',
+						name: 'element',
+						type: 'string',
+						default: '',
+						description: 'CSS selector to take screenshot for',
+					},
+					{
+						displayName: 'Full Page?',
+						name: 'fullPage',
+						type: 'boolean',
+						default: true,
+						description: 'Whether to take entire page or only visible part?',
+					},
+					{
+						displayName: 'Omit Background?',
+						name: 'omitBackground',
+						type: 'boolean',
+						default: false,
+						description:
+							'Whether to ignore default browser background (white) and use transparent?',
+					},
+					{
+						displayName: 'Scroll To',
+						name: 'scroll',
+						type: 'string',
+						default: '',
+						description: 'CSS selector of an element to scroll to',
+					},
+					{
+						displayName: 'Styles',
+						name: 'styles',
+						type: 'fixedCollection',
+						default: [],
+						description: 'CSS Styles to inject in the page (URL or inline)',
+						typeOptions: {
+							multipleValues: true,
+						},
+						options: [
+							{
+								displayName: 'Style',
+								name: 'style',
+								type: 'string',
+								default: '',
+								placeholder: 'Use a URL or inline CSS',
+							},
+						],
+					},
+					{
+						displayName: 'Headers',
+						name: 'headers',
+						type: 'fixedCollection',
+						typeOptions: {
+							multipleValues: true,
+						},
+						default: {
+							parameters: [
+								{
+									name: '',
+									value: '',
+								},
+							],
+						},
+						description: 'Add Headers',
+						options: [
+							{
+								name: 'parameters',
+								displayName: 'Parameter',
+								values: [
+									{
+										displayName: 'Name',
+										name: 'name',
+										type: 'string',
+										default: '',
+									},
+									{
+										displayName: 'Value',
+										name: 'value',
+										type: 'string',
+										default: '',
+									},
+								],
+							},
+						],
+					},
+				],
+			},
+		],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+
+		// tslint:disable-next-line: no-any
+		// tslint:disable-next-line: no-any
+		const binaryItems: INodeExecutionData[] = [];
+		const resource = this.getNodeParameter('resource', 0) as string;
+		const operation = this.getNodeParameter('operation', 0) as string;
+
+		const browser = createBrowserless({
+			timeout: 25000,
+			lossyDeviceName: true,
+			ignoreHTTPSErrors: true,
+		});
+
+		const context = await browser.createContext();
+
+		for (let i = 0; i < items.length; i++) {
+			const url = this.getNodeParameter('url', i) as string;
+			const options = this.getNodeParameter('options', i) as IDataObject;
+			const binaryItem: INodeExecutionData = {
+				json: {},
+				binary: {},
+			};
+
+			if (resource === 'page') {
+				if (operation === 'screenshot') {
+					// *********************************************************************
+					//                             Screenshot
+					// *********************************************************************
+					const opt = {
+						...(options.element && { element: options.element }),
+						...(options.device && { device: options.device }),
+						...(options.timeout && { timeout: options.timeout }),
+						...(options.userAgent && { userAgent: options.userAgent }),
+						...(options.fullPage && { fullPage: options.fullPage }),
+						...(options.omitBackground && { omitBackground: options.omitBackground }),
+						...(options.scroll && { scroll: options.scroll }),
+						viewport: {
+							...(options.width && { width: options.width }),
+							...(options.height && { height: options.height }),
+						},
+						headers: {},
+					};
+
+					// tslint:disable-next-line: no-any
+					if (options.headers && (options.headers as any)['parameters']) {
+						// tslint:disable-next-line: no-any
+						for (const header of (options.headers as any)['parameters']) {
+							// tslint:disable-next-line: no-any
+							(opt.headers as any)[header.name] = header.value;
+						}
+					}
+					console.dir(opt);
+					binaryItem.binary!['data'] = await this.helpers.prepareBinaryData(
+						await context.screenshot(url, {
+							...opt,
+							type: 'png',
+							encoding: 'binary',
+						}),
+						'screenshot.png',
+					);
+
+					binaryItems.push(binaryItem);
+				}
+			}
+		}
+
+		await browser.close();
+
+		return [binaryItems];
+	}
+}
